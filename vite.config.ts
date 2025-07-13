302550311ccb7ffd8d901ff997b94dcb37a25953
정은우 <136785138+Jung-eunwoo@users.noreply.github.com>
2025-07-13 17:55:56 +0900

    [CHORE] 깃허브 페이지 배포를 위한 action 파일 및 프로젝트 세팅 추가

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import vueDevTools from "vite-plugin-vue-devtools";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/pyeongwonArchitecture/",
+  base: process.env.NODE_ENV === "production" ? "/pyeongwonArchitecture/" : "/",
   plugins: [
     vue(),
     ...(process.env.NODE_ENV === "development" ? [vueDevTools()] : []),
