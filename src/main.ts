e5c9f6ba540f46055ef812a06748482de46bf843
정은우 <136785138+Jung-eunwoo@users.noreply.github.com>
2025-08-16 23:12:04 +0900

    [FIX] G-TAG 삽입 방법 변경

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,6 @@ import "./assets/main.css";
 
 import { createApp } from "vue";
 import App from "./App.vue";
-import { initGA } from "./utils/ga";
 import "./assets/main.css";
 
-initGA();
 createApp(App).mount("#app");
